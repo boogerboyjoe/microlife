70aec9a20106f68b34942089255dddc3c5ac1e65
NotReallyKeven <166420066+boogerboyjoe@users.noreply.github.com>
2026-05-29 12:29:06 -0400

    Made everything work
    Major refactoring of internal logic, now there are single celled meat and plant cells with dead plants too, next step is herbivores and predators.

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 #include <time.h>
 #include <math.h>
 
-#define TILE_SIZE 1
+#define TILE_SIZE 2
 #define CHUNK_SIZE 32
-#define CHUNK_NUMBER_X 35
-#define CHUNK_NUMBER_Y 35
+#define CHUNK_NUMBER_X 16
+#define CHUNK_NUMBER_Y 16
 #define MAX_ORGANISMS 10000
 #define MAX_TILES_PER_ORGANISM 1000
+#define MAX_PLANT_AGE 2
+#define MAX_MEAT_AGE 255
 
 #define TOTAL_CHUNKS (CHUNK_NUMBER_X * CHUNK_NUMBER_Y)
 #define TILES_PER_CHUNK (CHUNK_SIZE * CHUNK_SIZE)
@@ -27,11 +29,13 @@ typedef struct {
 typedef struct {
 	unsigned char type;
 	unsigned int id;
+	int age;
 }next_tile;
 
 typedef struct {
 	unsigned char type;
 	unsigned int id;
+	int age;
 }tile;
 
 typedef struct {
@@ -58,12 +62,14 @@ void initialize_world(void) {
 	for (int i = 0; i < TOTAL_TILES; i++) {
 		game_world.tiles[i].type = 0;
 		game_world.tiles[i].id = 0;
+		game_world.tiles[i].age = 0;
 		if (i == (WORLD_HEIGHT/2) * WORLD_WIDTH + (WORLD_WIDTH/2)) {
 			game_world.tiles[i].type = 2;
 			game_world.tiles[i].id = 0;
 		}
 		game_world.next_tiles[i].type = 0;
 		game_world.next_tiles[i].id = 0;
+		game_world.next_tiles[i].age = 0;
 	}
 }
 
@@ -79,17 +85,26 @@ void update_world(void) {
 			int tile_index = y * (WORLD_WIDTH)+x;
 			unsigned char type = game_world.tiles[tile_index].type;
 			unsigned int id = game_world.tiles[tile_index].id;
-
+			int age = game_world.tiles[tile_index].age;
+			int max_age = 0;
 			switch (type) {
 				case 2:
-					for (int i = 0; i < 3; i++) {
-						unsigned char expand_direction = rand() % 4;
-						// 0 = up, 1 = right, 2 = down, 3 = left
+					max_age = MAX_PLANT_AGE;
+					break;
+				case 4:
+					max_age = MAX_MEAT_AGE;
+			}
+			if (age <= max_age) {
+				switch (type) {
+					case 2:
+						for (int i = 0; i < 3; i++) {
+							unsigned char expand_direction = rand() % 4;
+							// 0 = up, 1 = right, 2 = down, 3 = left
 
-						int new_y = y;
-						int new_x = x;
+							int new_y = y;
+							int new_x = x;
 
-						switch (expand_direction) {
+							switch (expand_direction) {
 							case 0:
 								new_y = y - 1;
 								break;
@@ -100,31 +115,97 @@ void update_world(void) {
 								new_y = y + 1;
 								break;
 							default:
-								new_x = x - 1; 
-						}
-						if (new_x >= 0 && new_x < WORLD_WIDTH && new_y >= 0 && new_y < WORLD_HEIGHT) {
-							int new_tile_index = new_y * (WORLD_WIDTH)+ new_x;
-							unsigned char next_type = game_world.next_tiles[new_tile_index].type;
-
-							if (next_type == 0) {
-								game_world.next_tiles[new_tile_index].type = type;
-								game_world.next_tiles[new_tile_index].id = id;
+								new_x = x - 1;
 							}
-							i = 3;
+							if (new_x >= 0 && new_x < WORLD_WIDTH && new_y >= 0 && new_y < WORLD_HEIGHT) {
+								int new_tile_index = new_y * (WORLD_WIDTH)+new_x;
+								unsigned char next_type = game_world.tiles[new_tile_index].type;
+
+								if (next_type == 0) {
+									unsigned char mutation_chance = rand() % 200;
+									if (mutation_chance == 0) {
+										game_world.next_tiles[new_tile_index].type = 4;
+									}
+									else {
+										game_world.next_tiles[new_tile_index].type = type;
+									}
+									game_world.next_tiles[new_tile_index].id = id;
+									game_world.next_tiles[new_tile_index].age = 0;
+									i = 3;
+								}
+							}
 						}
-					}
-					break;
-				case 3:
-					
-					break;
+						game_world.next_tiles[tile_index].type = type;
+						game_world.next_tiles[tile_index].id = id;
+						game_world.next_tiles[tile_index].age = age + 1;
+						break;
+					case 3:
+						if (game_world.next_tiles[tile_index].type == 0) {
+							game_world.next_tiles[tile_index].type = type;
+							game_world.next_tiles[tile_index].id = id;
+							game_world.next_tiles[tile_index].age = age;
+						}
+						break;
+					case 4:
+						for (int i = 0; i < 3; i++) {
+							unsigned char expand_direction = rand() % 4;
+							// 0 = up, 1 = right, 2 = down, 3 = left
+
+							int new_y = y;
+							int new_x = x;
+
+							switch (expand_direction) {
+							case 0:
+								new_y = y - 1;
+								break;
+							case 1:
+								new_x = x + 1;
+								break;
+							case 2:
+								new_y = y + 1;
+								break;
+							default:
+								new_x = x - 1;
+							}
+							if (new_x >= 0 && new_x < WORLD_WIDTH && new_y >= 0 && new_y < WORLD_HEIGHT) {
+								int new_tile_index = new_y * (WORLD_WIDTH)+new_x;
+								unsigned char next_type = game_world.tiles[new_tile_index].type;
+
+								if (next_type == 0) {
+									game_world.next_tiles[new_tile_index].type = type;
+									game_world.next_tiles[new_tile_index].id = id;
+									game_world.next_tiles[new_tile_index].age = age + 1;
+									i = 3;
+								} else if (next_type == 3) {
+									game_world.next_tiles[tile_index].type = type;
+									game_world.next_tiles[tile_index].id = id;
+									game_world.next_tiles[tile_index].age = 0;
+									game_world.next_tiles[new_tile_index].type = type;
+									game_world.next_tiles[new_tile_index].id = id;
+									game_world.next_tiles[new_tile_index].age = age + 1;
+									i = 3;
+								}
+							}
+						}
+				}
+			} else {
+				if (type == 2) {
+					game_world.next_tiles[tile_index].type = 3;
+				} else {
+					game_world.next_tiles[tile_index].type = 0;
+				}
+				game_world.next_tiles[tile_index].id = 0;
+				game_world.next_tiles[tile_index].age = 0;
 			}
 		}
 	}
 	for (int i = 0; i < TOTAL_TILES; i++) {
 		game_world.tiles[i].type = game_world.next_tiles[i].type;
 		game_world.tiles[i].id = game_world.next_tiles[i].id;
+		game_world.tiles[i].age = game_world.next_tiles[i].age;
 		game_world.next_tiles[i].type = 0;
 		game_world.next_tiles[i].id = 0;
+		game_world.next_tiles[i].age = 0;
 	}
 }
 
@@ -153,6 +234,11 @@ void draw_screen(void) {
 				break;
 			case 3:
 				tile_color = (Color){ 60, 70, 5, 255 };
+
+				world_pixels[tile_index] = tile_color;
+				break;
+			case 4:
+				tile_color = (Color){ 140, 70, 5, 255 };
 
 				world_pixels[tile_index] = tile_color;
 				break;
